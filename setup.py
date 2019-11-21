7bba007468020626d60e485a57a1ae79d6941e32
Tema Klochko <klochko_as@bw-sw.com>
2019-11-21 17:54:24 +0700

    1367072 add 'grammar.txt' to package data

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='syntax-analyzer',
-    version='0.1',
+    version='0.1.1',
     description='Syntax Analyzer',
     url='https://github.com/datamonsters/syntax_analyzer',
     author='Data Monsters',
     author_email='support@datamonsters.co',
     packages=find_packages(),
     package_data={
-        'syntax_analyzer': ['dict/*'],
+        'syntax_analyzer': ['dict/*', 'grammar.txt'],
     },
     include_package_data=True,
     python_requires='>=3.6'
